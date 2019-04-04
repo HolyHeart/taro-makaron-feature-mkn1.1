6bb5b2310d2cb1af2b47c146942011a9c0a404fe
chenwenhua <wenhua.chen@versa-ai.com>
2019-04-04 10:18:53 +0800

    init

---
+++ src/services/api.config.ts
@@ -0,0 +1,40 @@
+// host域名管理
+import {ENV} from './config'
+const host = {
+  openapi: {
+    dev: 'https://openapi.dev.versa-ai.com',
+    prod: 'https://openapi.versa-ai.com'
+  },
+  miniapi: {
+    dev: 'https://mini-programdev.api.versa-ai.com',
+    prod: 'https://mini-program.api.versa-ai.com'
+  },
+  upload: {
+    dev: 'https://versa-static.oss-cn-shanghai.aliyuncs.com',
+    prod: 'https://versa-static.oss-cn-shanghai.aliyuncs.com'
+  },
+  download: {
+    dev: 'https://static01.versa-ai.com',
+    prod: 'https://static01.versa-ai.com'
+  }
+}
+
+// 获取域名
+function getHost (type = 'miniapi', ENV = 'dev') {
+  return host[type][ENV]
+}
+export const api = {
+  base: {
+    uploadToken: `${getHost('miniapi', ENV)}/upload/uploadPolicy`,
+    upload: `${getHost('miniapi', ENV)}/upload`,
+    auth: `${getHost('miniapi', ENV)}/user/auth/wechat/mini`,
+  },
+  core: {
+    segment: `${getHost('miniapi', ENV)}/segment/instance/original`,
+    column: `${getHost('miniapi', ENV)}/bg/column/theme/list`
+  }
+}
+export default {
+  name: 'api-config',
+  api,
+}
---
+++ src/services/config.ts
@@ -0,0 +1,13 @@
+// 配置文件
+export const ENV:string = 'dev'  // 'dev' 测试
+// export const ENV = 'prod'  // 'prod' 生产
+export const appId:string = 'wxcfe56965f4d986f0'
+export const appConfig:object = {
+  image_oss_postfix: '?x-oss-process=image/resize,h_748,w_560',
+  themeId: ENV === 'prod' ? '159718980238381056' : '189061533203746816' // 默认主题id
+}
+export default {
+  ENV,
+  appId: appId,
+  appConfig: appConfig
+}

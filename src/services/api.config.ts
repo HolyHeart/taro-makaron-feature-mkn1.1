0c83ce5f8bf89112f3720c6398059a16415b46ab
tiger <pengcheng.zhao@versa-ai.com>
2019-05-31 16:38:11 +0800

    feat: 调试小程序

--- src/services/api.config.ts
+++ src/services/api.config.ts
@@ -1,13 +1,13 @@
 // host域名管理
-import {ENV} from './config'
+import { ENV } from './config'
 const host = {
   openapi: {
     dev: 'https://openapi.dev.versa-ai.com',
     prod: 'https://openapi.versa-ai.com'
   },
   miniapi: {
-    dev: 'https://mini-programdev.api.versa-ai.com',
-    prod: 'https://mini-program.api.versa-ai.com'
+    dev: 'https://qq-mini-programdev.api.versa-ai.com',
+    prod: 'https://qq-mini-program.api.versa-ai.com'
   },
   upload: {
     dev: 'https://versa-static.oss-cn-shanghai.aliyuncs.com',
@@ -16,27 +16,31 @@ const host = {
   download: {
     dev: 'https://static01.versa-ai.com',
     prod: 'https://static01.versa-ai.com'
+  },
+  qq_miniapi: {
+    dev: 'https://qq-mini-program-dev.api.versa-ai.com',
+    prod: 'https://qq-mini-program.api.versa-ai.com'
   }
 }
 
 // 获取域名
-function getHost (type = 'miniapi', ENV = 'dev') {
+function getHost(type = 'miniapi', ENV = 'dev') {
   return host[type][ENV]
 }
 export const api = {
   base: {
-    uploadToken: `${getHost('miniapi', ENV)}/upload/uploadPolicy`,
-    upload: `${getHost('miniapi', ENV)}/upload`,
-    auth: `${getHost('miniapi', ENV)}/user/auth/wechat/mini`,
-    loginAuth: `${getHost('miniapi', ENV)}/user/auth/miniProgram`,
+    uploadToken: `${getHost('qq_miniapi', ENV)}/upload/uploadPolicy`,
+    upload: `${getHost('qq_miniapi', ENV)}/upload`,
+    auth: `${getHost('qq_miniapi', ENV)}/user/auth/session/qq/mini`,
+    loginAuth: `${getHost('qq_miniapi', ENV)}/user/auth/qqMiniProgram`,
   },
   core: {
-    segment: `${getHost('miniapi', ENV)}/segment/instance/original`,
-    column: `${getHost('miniapi', ENV)}/bg/column/theme/list`,
-    theme: `${getHost('miniapi', ENV)}/bg/:themeId/themeData`,
-    recommend: `${getHost('miniapi', ENV)}/scene/recommend`,
-    filterConvertVideo: `${getHost('miniapi', ENV)}/video/merge`,
-    reportFormId: `${getHost('miniapi', ENV)}/user/miniProgram/report`,
+    segment: `${getHost('qq_miniapi', ENV)}/segment/instance/original`,
+    column: `${getHost('qq_miniapi', ENV)}/bg/column/theme/list`,
+    theme: `${getHost('qq_miniapi', ENV)}/bg/:themeId/themeData`,
+    recommend: `${getHost('qq_miniapi', ENV)}/scene/recommend`,
+    filterConvertVideo: `${getHost('qq_miniapi', ENV)}/video/merge`,
+    reportFormId: `${getHost('qq_miniapi', ENV)}/user/miniProgram/report`,
   }
 }
 export default {

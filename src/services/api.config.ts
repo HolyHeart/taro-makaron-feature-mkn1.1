b0330901bcc34ff9f55faf4833430e3069f44ec4
tiger <tiger@tigerdeiMac-2.local>
2019-06-18 23:49:39 +0800

    init wechat branch

--- src/services/api.config.ts
+++ src/services/api.config.ts
@@ -29,18 +29,18 @@ function getHost(type = 'miniapi', ENV = 'dev') {
 }
 export const api = {
   base: {
-    uploadToken: `${getHost('qq_miniapi', ENV)}/upload/uploadPolicy`,
-    upload: `${getHost('qq_miniapi', ENV)}/upload`,
-    auth: `${getHost('qq_miniapi', ENV)}/user/auth/session/qq/mini`,
-    loginAuth: `${getHost('qq_miniapi', ENV)}/user/auth/qqMiniProgram`,
+    uploadToken: `${getHost('miniapi', ENV)}/upload/uploadPolicy`,
+    upload: `${getHost('miniapi', ENV)}/upload`,
+    auth: `${getHost('miniapi', ENV)}/user/auth/session/qq/mini`,
+    loginAuth: `${getHost('miniapi', ENV)}/user/auth/qqMiniProgram`,
   },
   core: {
-    segment: `${getHost('qq_miniapi', ENV)}/segment/instance/original`,
-    column: `${getHost('qq_miniapi', ENV)}/bg/column/theme/list`,
-    theme: `${getHost('qq_miniapi', ENV)}/bg/:themeId/themeData`,
-    recommend: `${getHost('qq_miniapi', ENV)}/scene/recommend`,
-    filterConvertVideo: `${getHost('qq_miniapi', ENV)}/video/merge`,
-    reportFormId: `${getHost('qq_miniapi', ENV)}/user/miniProgram/qq/report`,
+    segment: `${getHost('miniapi', ENV)}/segment/instance/original`,
+    column: `${getHost('miniapi', ENV)}/bg/column/theme/list`,
+    theme: `${getHost('miniapi', ENV)}/bg/:themeId/themeData`,
+    recommend: `${getHost('miniapi', ENV)}/scene/recommend`,
+    filterConvertVideo: `${getHost('miniapi', ENV)}/video/merge`,
+    reportFormId: `${getHost('miniapi', ENV)}/user/miniProgram/qq/report`,
   }
 }
 export default {

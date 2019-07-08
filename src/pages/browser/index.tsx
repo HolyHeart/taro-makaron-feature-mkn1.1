0f2ff0cd0071aa37fbdedc56013f068e32df04c4
mashichao <hzmashichao@163.com>
2019-07-08 19:54:38 +0800

    feat:完成基本布局

--- src/pages/browser/index.tsx
+++ src/pages/browser/index.tsx
@@ -1,6 +1,6 @@
 import { ComponentClass } from 'react'
 import Taro, { Component, Config } from '@tarojs/taro'
-import { View, Button, Image } from '@tarojs/components'
+import { View, Button, Image, ScrollView } from '@tarojs/components'
 import Title from '@/components/Title'
 import CustomIcon from '@/components/Icon'
 import './index.less'
@@ -30,21 +30,24 @@ class Browser extends Component {
   render () {
     return (
       <View className='browser'>
-        <Title
-          color="#333"
-          leftStyleObj={{left: Taro.pxTransform(8)}}
-          renderLeft={
-            <CustomIcon type="back" theme="dark" onClick={this.pageToHome}/>
-          }
-        >这图我能P</Title>
+        <View className='title'>
+          <Title
+            color="#333"
+            leftStyleObj={{left: Taro.pxTransform(8)}}
+            renderLeft={
+              <CustomIcon type="back" theme="dark" onClick={this.pageToHome}/>
+            }
+          >这图我能P</Title>
+        </View>
 
         <View className='navBar'>
         </View>
 
+        <ScrollView className='waterfall'>
+        </ScrollView>
 
-        <View className='waterfall'>
-        </View>
-        
+        <View className='divider'>-到底了哦-</View>
+
         <View className='btnGrp'>
           <Button className="button white" hoverClass="btn-hover">分享给好友</Button>
           <Button className="button pink" hoverClass="btn-hover">我要创作</Button>

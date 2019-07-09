67e033ce4659749dbf0efbb10f0bc8889a7fe063
mashichao <hzmashichao@163.com>
2019-07-09 17:10:46 +0800

    feat:下滑页面导航栏大小随之改变

--- src/pages/browser/index.tsx
+++ src/pages/browser/index.tsx
@@ -1,10 +1,10 @@
 import { ComponentClass } from 'react'
-import Taro, { Component, Config } from '@tarojs/taro'
-import { View, Button, Image, ScrollView } from '@tarojs/components'
+import Taro, { Component, Config, navigateBackMiniProgram } from '@tarojs/taro'
+import { View, Button, Image, ScrollView} from '@tarojs/components'
 import Title from '@/components/Title'
 import CustomIcon from '@/components/Icon'
 import './index.less'
-
+import globalData from "@/services/global_data"
 
 
 type PageOwnProps = {}
@@ -19,15 +19,57 @@ class Browser extends Component {
     enablePullDownRefresh:false
   }
   
+  state = {
+    navHeight: '',
+    navScrollHeight: '',
+  }
 
+  componentDidMount () {
+    this.getScreenHeight()
+  }
+
+  getScreenHeight () {
+    Taro.getSystemInfo({
+      success : res => 
+      globalData.sysHeight = res.screenHeight
+    })
+
+    this.setState({
+      navHeight: globalData.sysHeight * 0.6 + 'rpx',
+      navScrollHeight: globalData.sysHeight * 0.3 + 'rpx',
+    })
+  }
 
   pageToHome () {
     Taro.navigateBack({ delta: 1 })
   }
 
+  onPageScroll (e) {
+    var topDistance = e.scrollTop
+    var minHeight = globalData.sysHeight * 0.40
+    var curHeight = (globalData.sysHeight - topDistance) * 0.60
+    var navHeight = ''
+    var navScrollHeight = ''
+
+    if (curHeight <= minHeight) {
+      navHeight = minHeight + 'rpx'
+      navScrollHeight = minHeight/2 + 'rpx'
+    } else {
+      navHeight = curHeight + 'rpx'
+      navScrollHeight = curHeight/2 + 'rpx'
+    }
+
+    this.setState({
+      navHeight: navHeight,
+      navScrollHeight: navScrollHeight
+    })
+
+    console.log('curHei', curHeight, '|||', 'sysHei', globalData.sysHeight, '|||', 'topDis', topDistance, '|||', 'navHei', navHeight)
+  }
 
 
   render () {
+    
     return (
       <View className='browser'>
         <View className='title'>
@@ -40,13 +82,33 @@ class Browser extends Component {
           >这图我能P</Title>
         </View>
 
-        <View className='navBar'>
+        <View className='navBar' style={{height: this.state.navHeight}}>
+          <ScrollView className='scroll' scrollX={true} style={{height: this.state.navScrollHeight, marginTop: this.state.navScrollHeight}}>
+            <View className='item' style={{height: this.state.navScrollHeight, width: this.state.navScrollHeight}}> 
+            </View>
+            <View className='item' style={{height: this.state.navScrollHeight, width: this.state.navScrollHeight}}> 
+            </View>
+            <View className='item' style={{height: this.state.navScrollHeight, width: this.state.navScrollHeight}}> 
+            </View>
+            <View className='item' style={{height: this.state.navScrollHeight, width: this.state.navScrollHeight}}> 
+            </View>
+            <View className='item' style={{height: this.state.navScrollHeight, width: this.state.navScrollHeight}}> 
+            </View>
+            <View className='item' style={{height: this.state.navScrollHeight, width: this.state.navScrollHeight}}> 
+            </View>
+            <View className='item' style={{height: this.state.navScrollHeight, width: this.state.navScrollHeight}}> 
+            </View>
+            <View className='item' style={{height: this.state.navScrollHeight, width: this.state.navScrollHeight}}> 
+            </View>
+            <View className='item' style={{height: this.state.navScrollHeight, width: this.state.navScrollHeight}}> 
+            </View>
+          </ScrollView>
         </View>
 
         <ScrollView className='waterfall'>
         </ScrollView>
 
-        <View className='divider'>-到底了哦-</View>
+        <View className='divider'>-底部-</View>
 
         <View className='btnGrp'>
           <Button className="button white" hoverClass="btn-hover">分享给好友</Button>

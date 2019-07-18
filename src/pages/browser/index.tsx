783e9eb69f9c1346d4bc54a112781e3fa1a78671
mashichao <hzmashichao@163.com>
2019-07-18 10:52:48 +0800

    feat:功能化bottom tip

--- src/pages/browser/index.tsx
+++ src/pages/browser/index.tsx
@@ -124,6 +124,9 @@ class Browser extends Component {
       globalData.browserWorkList = workList
       //如果没有第二页
       const resultAdvance = await browser.psWorkList(globalData.totalUserInfo.uid, this.activityId, 2)
+
+      console.log(resultAdvance.result.result.workList.length, this.state.bottomTip)
+
       if (resultAdvance.result.result.workList.length === 0) {
         this.setState({
           bottomTip: '-没有更多啦-',
@@ -381,7 +384,7 @@ class Browser extends Component {
         </View>
       </View>
 
-      <View className='divider'>-没有更多啦-</View>
+      <View className='divider'>{this.state.bottomTip}</View>
       <View className='btnGrp'>
         <Button className="button white" hoverClass="btn-hover" openType='share'>分享给好友</Button>
         <Button className="button pink" hoverClass="btn-hover" onClick={this.goEditor}>我要创作</Button>

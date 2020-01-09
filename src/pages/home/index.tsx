8a3194996b161d0a3863f3bfe0ede78f4c184a92
tiger <tiger@KevindeiMac.local>
2020-01-09 15:14:38 +0800

    fixbug

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -286,17 +286,21 @@ class Home extends Component {
   }
 
   handleGetUserInfo = (scene, type, originalImage) => {
+    this.app.aldstat.sendEvent('授权订阅', '开启授权订阅')
     wx.requestSubscribeMessage({
       tmplIds: ['eAj0pOYsLUn2bo-VXJZLiU01P7_PJH_BoiEczQgdhec'],
       success: (res) => {
          console.log(res)
           if(res['eAj0pOYsLUn2bo-VXJZLiU01P7_PJH_BoiEczQgdhec'] ==='accept'){
+            this.app.aldstat.sendEvent('授权订阅', '允许授权订阅')
             base.subScribe({
               "appId": 'wxcfe56965f4d986f0',
               "sessionId":Taro.getStorageSync('session'),
               "openId": Taro.getStorageSync('openId'),
               "templateIds": "eAj0pOYsLUn2bo-VXJZLiU01P7_PJH_BoiEczQgdhec"
             })
+          }else{
+            this.app.aldstat.sendEvent('授权订阅', '不允许授权订阅')
           }
         this.goScene(scene, type, originalImage)
       },

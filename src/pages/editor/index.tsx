cca7af328aa405ca15f3a8d1b40fb5380d4687d5
mao <linlin@tiger.local>
2020-04-02 20:38:30 +0800

    fix

--- src/pages/editor/index.tsx
+++ src/pages/editor/index.tsx
@@ -244,7 +244,7 @@ class Editor extends Component {
       }
     }
     // console.log('789',title, path, shareImageUrl)
-    Taro.navigateTo({ url: `/pages/index?shareSource=${shareImageUrl}` })
+    // Taro.navigateTo({ url: `/pages/index?shareSource=${shareImageUrl}` })
     return {
       title: title,
       path: path,
@@ -1542,9 +1542,9 @@ class Editor extends Component {
           <MarginTopWrap config={{ large: 60, small: 40, default: 20 }} >
             <View style="display:flex;margin-top:120rpx">
               <Button style='flex:1;z-index:2' id='addPhoto' openType="getUserInfo" className="custom-button pink" hoverClass="btn-hover" onGetUserInfo={this.todo}>{this.state.chooseText}</Button>
-              {/* {Taro.getStorageSync('saveNumber').number ===0 ?  */}
+              {Taro.getStorageSync('saveNumber').number ===0 ? 
               <Button style='flex:1;margin-left:10px' className="custom-button white" hoverClass="btn-hover" onClick={this.handleOpenResult} openType="share">分享并保存</Button> 
-            {/* :<Button style='flex:1;margin-left:10px' className="custom-button white" hoverClass="btn-hover" onClick={this.saveImg}>保存</Button>}  */}
+            :<Button style='flex:1;margin-left:10px' className="custom-button white" hoverClass="btn-hover" onClick={this.saveImg}>保存</Button>} 
             </View>
           </MarginTopWrap>
           {this.state.isshow === true ? <Dialog 

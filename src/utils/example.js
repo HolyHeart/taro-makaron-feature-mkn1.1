669c5c60e3afc34f77104a1ab29f3454c2421769
dingmingjie <mingjie.ding@versa-ai.com>
2020-07-02 12:51:06 +0800

    feat: update

--- src/utils/example.js
+++ src/utils/example.js
@@ -1,8 +1,11 @@
 import { OrbitControls } from './jsm/controls/OrbitControls'
 import gLTF from './jsm/loaders/GLTFLoader'
-import url from '../assets/card.gltf'
 
-export default function renderExample1(canvas, THREE) {
+let objCache = null
+let canvasCache = null
+let THREECache = null
+
+export function renderExample1(canvas, THREE, obj, url) {
   var camera, scene, renderer
   var cube
   init()
@@ -20,27 +23,52 @@ export default function renderExample1(canvas, THREE) {
     renderer = new THREE.WebGLRenderer({ antialias: true })
     renderer.setPixelRatio(wx.getSystemInfoSync().pixelRatio)
     renderer.setSize(canvas.width, canvas.height)
+    let geometry = new THREE.BoxGeometry(10, 10, 10)
 
-    let GLTFLoader = gLTF(THREE)
-    var geometry = new THREE.BoxGeometry(10, 10, 10)
-    const gltfLoader = new GLTFLoader()
-    gltfLoader.load(
-      'https://activity-dev.versa-ai.com/cardB.gltf',
-      (gltf) => {
-        gltf.scene.traverse(function (child) {
-          if (child.isMesh) {
-            child.material.emissive = child.material.color
-            child.material.emissiveMap = child.material.map
-          }
-        })
-        const root = gltf.scene
-        console.log(root, 123123)
-        scene.add(root)
-      },
-      (e) => {
-        console.log(e)
-      },
-    )
+    if (!url) {
+      let GLTFLoader = gLTF(THREE)
+      const gltfLoader = new GLTFLoader()
+      gltfLoader.load(
+        'https://activity-dev.versa-ai.com/cardB.gltf',
+        (gltf) => {
+          gltf.scene.traverse(function (child) {
+            if (child.isMesh) {
+              child.material.emissive = child.material.color
+              child.material.emissiveMap = child.material.map
+            }
+          })
+          const root = gltf.scene
+          console.log(
+            root,
+            123123,
+            root.children[0].material.map.image.currentSrc,
+          )
+          objCache = root
+          canvasCache = canvas
+          THREECache = THREE
+          scene.add(root)
+        },
+        (e) => {
+          console.log(e)
+        },
+      )
+    } else {
+      objCache.traverse(function (child) {
+        if (child.isMesh) {
+          child.material.emissive = child.material.color
+          child.material.emissiveMap = child.material.map
+        }
+      })
+      //   objCache.children[0].material.map.image.currentSrc = url
+      let material = new THREE.MeshBasicMaterial({
+        map: THREE.ImageUtils.loadTexture(
+          'https://static01.versa-ai.com/versa-website/static/img/src/pages/offical/pc/index/img/-view-11.cfa33dc.jpg',
+        ),
+      })
+      objCache.children[0].material = material
+      scene.add(objCache)
+    }
+
     var material = new THREE.MeshBasicMaterial({ color: 0xed3ed3 })
     cube = new THREE.Mesh(geometry, material)
     // scene.add(cube)
@@ -58,4 +86,8 @@ export default function renderExample1(canvas, THREE) {
     canvas.requestAnimationFrame(animate)
     renderer.render(scene, camera)
   }
+}
+
+export function change(url) {
+  renderExample1(canvasCache, THREECache, objCache, url)
 }

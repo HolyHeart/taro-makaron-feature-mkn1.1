02f591f5e81ec703e0e7ad74ca5bb3ec28f9f81e
dingmingjie <mingjie.ding@versa-ai.com>
2020-07-02 22:45:22 +0800

    feat: update

--- src/utils/example.js
+++ src/utils/example.js
@@ -4,9 +4,11 @@ import gLTF from './jsm/loaders/GLTFLoader'
 let objCache = null
 let canvasCache = null
 let THREECache = null
+let rendererCache = null
+let timer
 
 export function renderExample1(canvas, THREE, obj, url) {
-  var camera, scene, renderer
+  let camera, scene, renderer
   var cube
   init()
   animate()
@@ -20,9 +22,15 @@ export function renderExample1(canvas, THREE, obj, url) {
     scene = new THREE.Scene()
     scene.background = new THREE.Color(0xf0f0f0)
 
-    renderer = new THREE.WebGLRenderer({ antialias: true })
-    renderer.setPixelRatio(wx.getSystemInfoSync().pixelRatio)
-    renderer.setSize(canvas.width, canvas.height)
+    if (!url) {
+      renderer = new THREE.WebGLRenderer({ antialias: true })
+      renderer.setPixelRatio(wx.getSystemInfoSync().pixelRatio)
+      renderer.setSize(canvas.width, canvas.height)
+      rendererCache = renderer
+    } else {
+      renderer = rendererCache
+    }
+
     let geometry = new THREE.BoxGeometry(10, 10, 10)
 
     if (!url) {
@@ -38,11 +46,6 @@ export function renderExample1(canvas, THREE, obj, url) {
             }
           })
           const root = gltf.scene
-          console.log(
-            root,
-            123123,
-            root.children[0].material.map.image.currentSrc,
-          )
           objCache = root
           canvasCache = canvas
           THREECache = THREE
@@ -53,24 +56,68 @@ export function renderExample1(canvas, THREE, obj, url) {
         },
       )
     } else {
-      objCache.traverse(function (child) {
-        if (child.isMesh) {
-          child.material.emissive = child.material.color
-          child.material.emissiveMap = child.material.map
-        }
-      })
-      //   objCache.children[0].material.map.image.currentSrc = url
-      let material = new THREE.MeshBasicMaterial({
-        map: THREE.ImageUtils.loadTexture(
-          'https://static01.versa-ai.com/versa-website/static/img/src/pages/offical/pc/index/img/-view-11.cfa33dc.jpg',
-        ),
-      })
-      objCache.children[0].material = material
-      scene.add(objCache)
+      //   let material = new THREE.MeshBasicMaterial({
+      //     map: THREE.ImageUtils.loadTexture(
+      //       'https://static01.versa-ai.com/versa-website/static/img/src/pages/offical/pc/index/img/-view-11.cfa33dc.jpg',
+      //     ),
+      //   })
+      //   objCache.traverse(function (child) {
+      //     if (child.isMesh) {
+      //       child.material.emissive = child.material.color
+      //       child.material.emissiveMap = child.material.map
+      //       child.material.needsUpdate = true
+      //     }
+      //   })
+
+      //   objCache.children[0].material = material
+      //   scene.add(objCache)
+
+      const texture = new THREE.TextureLoader().load(
+        require('../assets/images/avatar.jpg'),
+        (texture) => {
+          //   console.log(texture, 'texture', objCache.children[0].material.map)
+          texture.wrapS = THREE.RepeatWrapping
+          texture.wrapT = THREE.RepeatWrapping
+          try {
+            texture.image.currentSrc = texture.image.src
+          } catch (e) {
+            console.log(e)
+          }
+          console.log(texture.image.currentSrc, 'src')
+
+          objCache.traverse(function (child) {
+            if (child.isMesh) {
+              //   child.material.map = texture
+              child.material.emissive = child.material.color
+              child.material.emissiveMap = texture
+              child.material.map = texture
+              //   child.material.image = texture.image
+            }
+          })
+          console.log(
+            objCache,
+            objCache.children[0].material.map.image.currentSrc,
+            'aaa',
+          )
+          //   objCache.children[0].material.map = texture
+          //   console.log(
+          //     objCache,
+          //     'show',
+          //     objCache.children[0].material.map.image.currentSrc,
+          //   )
+          scene.add(objCache)
+        },
+        (err) => {
+          console.error(err)
+        },
+      )
+      //   const material = new THREE.MeshBasicMaterial({ map: texture })
+      //   objCache.children[0].material = material
+      //   scene.add(objCache)
     }
 
-    var material = new THREE.MeshBasicMaterial({ color: 0xed3ed3 })
-    cube = new THREE.Mesh(geometry, material)
+    // var material = new THREE.MeshBasicMaterial({ color: 0xed3ed3 })
+    // cube = new THREE.Mesh(geometry, material)
     // scene.add(cube)
     const controls = new OrbitControls(camera, renderer.domElement)
     controls.update()
@@ -81,13 +128,14 @@ export function renderExample1(canvas, THREE, obj, url) {
     camera.position.z = 30
   }
   function animate() {
-    cube.rotation.x += 0.01
-    cube.rotation.y += 0.01
-    canvas.requestAnimationFrame(animate)
+    cube && (cube.rotation.x += 0.01)
+    cube && (cube.rotation.y += 0.01)
+    timer = canvas.requestAnimationFrame(animate)
     renderer.render(scene, camera)
   }
 }
 
 export function change(url) {
-  renderExample1(canvasCache, THREECache, objCache, url)
+  canvasCache.cancelAnimationFrame(timer)
+  renderExample1(canvasCache, THREECache, objCache, 1)
 }

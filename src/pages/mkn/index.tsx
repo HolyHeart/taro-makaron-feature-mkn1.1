1c0f00565d9d5431ecc9d394c4f044284e4acfe3
yjzeng <zyjcmd@163.com>
2020-08-10 16:00:35 +0800

    feat:update

--- src/pages/mkn/index.tsx
+++ src/pages/mkn/index.tsx
@@ -471,10 +471,10 @@ class Editor extends Component {
         console.log("技术犯规了");
         // work.pageToError();
         Taro.showToast({
-          title: '没有找到人物',
-          icon: 'none',
-          duration: 2000
-        })
+          title: "没有找到人物",
+          icon: "none",
+          duration: 2000,
+        });
         return;
       }
     } catch (err) {
@@ -905,12 +905,15 @@ class Editor extends Component {
     // 下载远程背景图片
     let localBgImagePath = "";
     try {
-      const bgUrl = currentScene.bgUrl + postfix;
-      localBgImagePath = await this.downloadRemoteImage(bgUrl);
+      //const bgUrl = currentScene.bgUrl + postfix;
+      localBgImagePath = currentScene.bgUrl;
+      console.log(localBgImagePath), "this is local bgUrl checking checking");
+      //localBgImagePath = await this.downloadRemoteImage(bgUrl);
     } catch (err) {
       console.log("下载背景图片失败", err);
       return;
     }
+    //localBgImagePath = currentScene.bgUrl;
     //防止锯齿，绘的图片是所需图片的3倍
     context.drawImage(
       localBgImagePath,
@@ -1055,7 +1058,7 @@ class Editor extends Component {
     } else {
       try {
         const result = await service.base.downloadFile(remoteUrl);
-        localImagePath = result.tempFilePath || remoteUrl ;
+        localImagePath = result.tempFilePath || remoteUrl;
       } catch (err) {
         console.log("下载图片失败", err);
       }
@@ -1462,7 +1465,7 @@ class Editor extends Component {
               if (this.selectedItem.id.indexOf("foreground") !== -1) {
                 const separateResult = (globalData.separateResult = await this.initSegment());
                 let res = await this.initSeparateData(separateResult);
-                if(res && res.separateUrl){
+                if (res && res.separateUrl) {
                   path = res.separateUrl;
                 }
                 this.uploadCoverImg(path);
@@ -2102,13 +2105,13 @@ class Editor extends Component {
   showPicList() {
     let coverList = [...this.state.coverList];
     let needAutoActive = true;
-    for(let i = 0; i < coverList.length; i++){
-      if(coverList[i].isActive){
+    for (let i = 0; i < coverList.length; i++) {
+      if (coverList[i].isActive) {
         needAutoActive = false;
         break;
       }
     }
-    if(needAutoActive){
+    if (needAutoActive) {
       coverList[0].isActive = true;
       coverList[0].fixed = false;
     }
@@ -2148,7 +2151,7 @@ class Editor extends Component {
     let sessionId = Session.get();
     let str = `${getHost("miniapi", ENV)}/word/sticker/picture?text=${encodeURI(
       data
-    )}&fontPackageUrl=${fontPackageUrl}&fontSize=${fontSize}&sessionId=${sessionId}&deviceId=${deviceId}`;
+    )}&fontPackageUrl=${fontPackageUrl}&fontSize=${fontSize}&sessionId=${sessionId}&deviceId=${deviceId}&fontColor=${fontColor}`;
     return str;
   }
 
@@ -2227,7 +2230,12 @@ class Editor extends Component {
                 className={`crop`}
                 id="crop"
               >
-                {showType === 0 && <Image className='thumbnail_image' src={currentScene.thumbnailUrl}/>}
+                {showType === 0 && (
+                  <Image
+                    className="thumbnail_image"
+                    src={currentScene.thumbnailUrl}
+                  />
+                )}
                 <View className="background-image">
                   <Image
                     src={currentScene.bgUrl}
